0ebd03eaaa39cb8929c7fe4488cd3fbd65969ce5
Clayton Andrews <claytonandrews897@gmail.com>
2024-02-14 14:43:23 -0500

    removed comments

--- script.js
+++ script.js
@@ -9,23 +9,18 @@ var venueLng;
 var item;
 var userCity;
 
-// function getLocation() {
-//     navigator.geolocation.getCurrentPosition(showPosition, showError);
-// }
-
 function showPosition(data) {
   var token = "6417bd03e4fe33";
-  // API endpoint for geolocation
+ 
   var apiEndpoint = `https://ipinfo.io/json?token=${token}`;
 
-  // Make a request to the API
+  
   fetch(apiEndpoint)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      // Log the API response to the console
-      // use the data to pull specifics
+     
       gloablLat = data.loc.split(",")[0];
       globalLon = data.loc.split(",")[1];
       userCity = data.city;
@@ -36,10 +31,7 @@ function showPosition(data) {
       console.log("Longitude: " + globalLon);
       console.log("Geolocation API Response:", data);
 
-      // var x = document.getElementById("location");
-      // x.innerHTML = "Latitude: " + gloablLat +
-      // "<br>Longitude: " + globalLon;
-
+     
       $.ajax({
         type: "GET",
         url:
@@ -51,14 +43,12 @@ function showPosition(data) {
         success: function (json) {
           console.log(json);
           console.log(json.page.totalElements + " events found.");
-        //   console.log(response.json)
+        
           var e = document.getElementById("events");
-        //   e.innerHTML = " events found";
-        //   json.page.totalElements + " events found.";
-        //   showPosition();
+        
           showEvents(json);
           initMap(data, json);
-        //   fetchNearbyPlaces(latlon);
+       
         },
         error: function (xhr, status, err) {
           console.log(err);
@@ -89,7 +79,7 @@ function showError(error) {
 var item;
 
 function showEvents(json) {
-  // Calculate distances and store events with distance
+  
   var eventsWithDistances = json._embedded.events.map(function (event) {
     var venue = event._embedded.venues[0];
     var distance = haversineDistance(
@@ -106,12 +96,11 @@ function showEvents(json) {
     return item;
   });
 
-  // Sort events by distance
+  
   eventsWithDistances.sort(function (a, b) {
     return parseFloat(a.distance) - parseFloat(b.distance);
   });
 
-  //sort by date
   eventsWithDistances.sort(function (a, b) {
     return (
       new Date(a.event.dates.start.dateTime) -
@@ -119,7 +108,7 @@ function showEvents(json) {
     );
   });
 
-  // Append sorted events to the DOM
+  
   eventsWithDistances.forEach(function (item) {
     var event = item.event;
     var venue = event._embedded.venues[0];
@@ -167,35 +156,12 @@ function showEvents(json) {
             </div>
         `;
 
-    // <article class="media">
-    //     <figure class="media-left">
-    //         <p class="image is-128x128">
-    //             <img src="${imageUrl}" alt="${event.name}">
-    //         </p>
-    //     </figure>
-    //     <div class="media-content">
-    //         <div class="content">
-    //             <p>
-    //                 <strong>${event.name}</strong><br>
-    //                 <small>${venue.name}, ${venue.city.name}</small><br>
-    //                 <small>${distance} Miles away</small><br>
-    //                 <small>${date}</small><br>
-    //                 <small>Price Range: ${priceRange}</small><br>
-    //                 <small>${parkingInfo}</small>
-    //             </p>
-    //         </div>
-    //         <nav class="level is-mobile">
-    //             <div class="level-right">
-    //                 <a class="button is-info" href="${event.url}" target="_blank">Get Tickets</a>
-    //             </div>
-    //         </nav>
-    //     </div>
-    // </article>`;
+
 
     $(".carousel").append(eventHtml);
   });
 
-  //init bulma carousel
+
   bulmaCarousel.attach(".carousel", {
     slidesToShow: 1,
     slidesToScroll: 1,
@@ -212,8 +178,8 @@ function showEvents(json) {
 }
 
 function haversineDistance(lat1, lon1, lat2, lon2) {
-  const R = 6371e3; // Earth's radius in meters
-  const phi1 = (lat1 * Math.PI) / 180; // Convert degrees to radians
+  const R = 6371e3; 
+  const phi1 = (lat1 * Math.PI) / 180; 
   const phi2 = (lat2 * Math.PI) / 180;
   const deltaPhi = ((lat2 - lat1) * Math.PI) / 180;
   const deltaLambda = ((lon2 - lon1) * Math.PI) / 180;
@@ -226,8 +192,8 @@ function haversineDistance(lat1, lon1, lat2, lon2) {
       Math.sin(deltaLambda / 2);
   const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
-  const d = R * c; // Distance in meters
-  const dInMiles = d / 1609.34; // Convert meters to miles
+  const d = R * c; 
+  const dInMiles = d / 1609.34; 
   return dInMiles;
 }
 
@@ -254,60 +220,4 @@ function addMarker(map, event) {
   marker.setIcon("http://maps.google.com/mapfiles/ms/icons/red-dot.png");
   console.log(marker);
 }
-// var data;
 
-// function getWeather(lat, lon, callback){
-//     var apiKey = '9fda455ae9137822224a160754647dd2';
-//     var forecastApi = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
-
-//     var lat = venue.location.latitude
-//     var lon = venue.location.longitude
-
-//     fetch(forecastApi)
-//     .then(function(response) {
-//         return response.json();
-//     })
-//     .then(function(data) {
-//         // Example of logging the forecast data
-//         console.log(data);
-//         data.forEach(function(forecastEntry) {
-//             // Get the weather description and icon code
-//             var weatherDescription = forecastEntry.weather[0].description;
-//             var iconCode = forecastEntry.weather[0].icon;
-
-//             // Construct the icon URL using the icon code
-//             var iconUrl = `http://openweathermap.org/img/wn/${iconCode}.png`;
-
-//             // Create an HTML element to display the weather icon and description
-//             var weatherElement = document.createElement('div');
-//             weatherElement.innerHTML = `<img src="${iconUrl}" alt="${weatherDescription}"> ${weatherDescription}`;
-
-//             // Add the weather element to the DOM where you want to display the forecast
-//         });
-
-//         // Extract and display the forecast information here
-//         // For instance, you could loop through the list array and create HTML elements to display the forecast
-//     });
-// }
-
-// // Assuming `forecastData` is the JSON object returned from the API call
-// // data.forEach(function(forecastEntry) {
-// //     // Get the weather description and icon code
-// //     var weatherDescription = forecastEntry.weather[0].description;
-// //     var iconCode = forecastEntry.weather[0].icon;
-
-// //     // Construct the icon URL using the icon code
-// //     var iconUrl = `http://openweathermap.org/img/wn/${iconCode}.png`;
-
-// //     // Create an HTML element to display the weather icon and description
-// //     var weatherElement = document.createElement('div');
-// //     weatherElement.innerHTML = `<img src="${iconUrl}" alt="${weatherDescription}"> ${weatherDescription}`;
-
-// //     // Add the weather element to the DOM where you want to display the forecast
-// // });
-
-showPosition();
-// getWeather(venue.location.latitude, venue.location.longitude)
-
-//192.168.1.12
-// google maps key: AIzaSyCj3wvLnBaKeIBdhCqaNrp14KyEq9KB1pY
